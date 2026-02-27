7f7f24b987df658aad1ee49772594e12c44711ed
esobirjon2197 <esobirjon2197@gmail.com>
2026-02-27 16:51:20 +0500

    Add multiple functions for various tasks

---
+++ main.py
@@ -0,0 +1,387 @@
+# 1-m
+def salomlash():
+    print("Salom dunyo")
+
+salomlash()
+
+
+# 2-m
+def raqam():
+    print(100)
+
+raqam()
+
+
+# 3-m
+def mativatsiya():
+    print("Siz muvaffaqiyat sari yurasiz!")
+
+mativatsiya()
+
+
+# 4-m
+def takror():
+    print("Python" * 3)
+
+takror()
+
+
+# 5-m
+def qosh():
+    print(5 + 7)
+
+qosh()
+
+
+# 6-m
+def vaqt():
+    print("Hozir tong")
+
+vaqt()
+
+
+# 7-m
+def fikr():
+    print("O'rganish - kelajak")
+
+fikr()
+
+
+# 8-m
+def bosh():
+    print("Dastur ishga tushdi!")
+
+bosh()
+
+
+# 9-m
+def tasadif():
+    print("Bugun yaxshi kun")
+
+tasadif()
+
+
+# 10-m
+def oddiy():
+    print("Xush kelibsiz")
+
+oddiy()
+
+
+
+# 1-m
+def ism(a):
+    print("SAlom", a)
+
+ism("Sobirjon")
+
+
+# 2-m
+def raqam(a):
+    print("son", a)
+
+raqam(10)
+
+
+# 3-m
+def qosh(a, b):
+    print(a + b)
+
+qosh(2, 2)
+
+
+# 4-m
+def ism(a, b):
+    print("Ism va yosh", a, b)
+
+ism("Sobirjon", 15)
+
+
+# 5-m
+def kvdrat(a):
+    print(a * a)
+
+kvdrat(2)
+
+
+# 6-m
+def matn(a, b):
+    print(a * b)
+
+matn("python", 2)
+
+
+# 7-m
+def farq(a, b):
+    print(a - b)
+
+farq(10, 5)
+
+
+# 8-m
+def ism(a):
+    print("Hurmatli", a)
+
+ism("Bunyod")
+
+
+# 9-m
+def kop(a, b):
+    print(a * b)
+
+kop(2, 10)
+
+
+# 10-m
+def bol(a, b):
+    print(a / b)
+
+bol(20, 2)
+
+
+
+# 1-m
+def summa(a, b):
+    c = a + b
+    return c
+
+c = summa(2, 5)
+print(c)
+
+
+# 2-m
+def kvadrat(a):
+    return a * a
+
+x = kvadrat(6)
+print(x)
+
+
+# 3-m
+def ism(i):
+    return len(i)
+
+a = ism("Sobirjon")
+print(a)
+
+
+# 4-m
+def maxsimum(i, a):
+    return max(i, a)
+
+b = maxsimum(12, 4)
+print(b)
+
+
+# 5-m
+def matn(i):
+    return matn(i)
+
+
+a = input("matn kirit: ")
+print(f"Sizning matningiz: {a}")
+
+
+# 6-m
+def farq(a, b):
+    return a - b
+
+i = farq(2, 10)
+print(i)
+
+
+# 7-m
+def kop(a, b):
+    return a * b
+
+i = kop(2, 3)
+print(i)
+
+
+# 8-m
+def bol(a, b):
+    return a / b
+
+i = bol(6, 3)
+print(i)
+
+
+# 10-m
+def salom(i):
+    return i
+
+b = salom('Salom')
+print(b)
+
+
+
+
+# 1-m
+def ism(a="Bunyod"):
+    return a
+
+i = ism()
+print("Salom", i)
+
+
+# 2-m
+def daraja(a, b):
+    return a ** b
+
+i = daraja(2, 2)
+print(i)
+
+
+# 3-m
+def takaror(a, b):
+    return a * b
+
+i = takaror("Bunyod", 999)
+print(i)
+
+
+# 4-m
+def ism(a, b):
+    return a, b
+
+i = ism("Shohsanam", 99)
+print(i)
+
+
+# 5-m
+def summa(a, b):
+    return a + b
+
+i = summa(2, 10)
+print(i)
+
+
+# 6-m
+def summa(a, b):
+    return a * b
+
+i = summa(2, 1)
+print(i)
+
+
+
+# 7-m
+def summa(a, b):
+    return a * b
+
+i = summa(2, 1)
+print(i)
+
+
+# 8-m
+def raqam(a, b):
+    return a > b
+
+i = raqam(11, 10)
+print(i)
+
+
+# 9-m
+def qosh(a, b):
+    return a + b
+
+i = qosh("Bunyod", "Sobirjon")
+print(i)
+
+
+# 10-m
+def salom(a):
+    return f"Xush kelib siz: {a}"
+
+i = salom("Bunyod")
+print(i)
+
+
+# 1-m
+def juftmi(a, b):
+    return a == b
+
+i = juftmi(2, 2)
+print(i)
+
+
+# 2-m
+def teng(a, b):
+    return a == b
+
+i = teng(1, 3)
+print(i)
+
+
+# 3-m
+def matn(a):
+    return a.upper()
+
+i = matn("asaidj")
+print(i)
+
+
+# 4-m
+def matn(a):
+    return a.lower()
+
+a = matn("ayhug")
+print(a)
+
+
+# 5-m
+def lenn(a):
+    return len(a)
+a = lenn("jhasfkjaaaaaadgbzfsyhwgefgyewfbcyjadfsjafvgdjsjasssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffjjfffffffffffffffffffffffffffffffffffjfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffjffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffjfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffjfffffjfjfjfjfjfjfjfjfjfjfjfjjfjfjfjfjfjfjfjfjfjfjfjfjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjfffffffffffffffffffffffffffffffjjjjjjjjjjfjjjjj")
+
+print(a)
+
+
+# 6-m
+def yigindi_3r(a, b, c):
+    return  a + b + c
+
+a = yigindi_3r(3,4,4)
+print(a)
+
+
+# 7-m
+def orqa_summ(s, b):
+    natija = (s + b) / 2
+    return natija
+
+a = orqa_summ(4, 8)
+print(a)
+
+
+# 8-m
+def len(a):
+    return len(a)
+
+a = len("jhasfkjaaaaaadgbzfsyhwgefgyewfbcyjadfsjafvgdjsjasssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffjjfffffffffffffffffffffffffffffffffffjfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffjffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffjfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffjfffffjfjfjfjfjfjfjfjfjfjfjfjjfjfjfjfjfjfjfjfjfjfjfjfjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjfffffffffffffffffffffffffffffffjjjjjjjjjjfjjjjj")
+print(a)
+
+
+# 9-m
+def katta_r(a, b, c):
+    return max(a, b, c)
+
+a = katta_r(4, 8, 872358685462565635456674276247762674254625426)
+print(a)
+
+
+# 10-m
+def tabrik(a, b, c="Tabriklaymiz"):
+    return a, b, c
+
+a = tabrik("Bunyod", "Jigar",)
+print(a)
+
+
+
+
+
+
+
+
+
